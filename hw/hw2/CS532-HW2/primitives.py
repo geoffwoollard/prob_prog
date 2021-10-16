655523781ae88201538c38c13146d86937cfb3fb
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-10-16 15:05:32 -0700

    get primitive passing test. primitive dictionary moved to primitive.py

--- hw/hw2/CS532-HW2/primitives.py
+++ hw/hw2/CS532-HW2/primitives.py
@@ -1,3 +1,15 @@
 import torch
 
-#TODO
+def get_primitive(vector_and_index):
+    vector, index = vector_and_index
+    return vector[index.long()]
+
+primitives_d = {
+    '/': lambda x: torch.divide(x[0], x[1]),
+    '*': lambda x: torch.multiply(x[0], x[1]),
+    '+': lambda x: torch.add(x[0], x[1]),
+    '-': lambda x: torch.subtract(x[0],x[1]),
+    'sqrt': lambda x: torch.sqrt(x[0]),
+    'vector': torch.tensor,
+    'get' : get_primitive,
+}
