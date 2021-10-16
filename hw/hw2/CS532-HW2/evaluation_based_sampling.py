2836765fc0cb367f2195edfa398255d0f9f089a1
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-10-16 14:53:41 -0700

    vector primitive passing test

--- hw/hw2/CS532-HW2/evaluation_based_sampling.py
+++ hw/hw2/CS532-HW2/evaluation_based_sampling.py
@@ -10,6 +10,7 @@ primitives = {
     '+': lambda x: torch.add(x[0], x[1]),
     '-': lambda x: torch.subtract(x[0],x[1]),
     'sqrt': lambda x: torch.sqrt(x[0]),
+    'vector': torch.tensor,
 }
 
 number = (int,float)
@@ -55,7 +56,7 @@ def get_stream(ast):
 
 def run_deterministic_tests():
     
-    for i in range(1,5):
+    for i in range(6,7):
         #note: this path should be with respect to the daphne path!
         os.chdir('/Users/gw/repos/prob_prog/hw/hw2/CS532-HW2/')
         ast = daphne(['desugar', '-i', '../prob_prog/hw/hw2/CS532-HW2/programs/tests/deterministic/test_{}.daphne'.format(i)])
