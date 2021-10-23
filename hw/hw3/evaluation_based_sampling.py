c7f03193e470982e423beff9c1ffad47a2e6a6d1
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-10-22 17:58:12 -0700

    sigma side effects. observe log prob working. refactored evaluate_program internally. evaluate now returns sigma

--- hw/hw3/evaluation_based_sampling.py
+++ hw/hw3/evaluation_based_sampling.py
@@ -24,7 +24,7 @@ logger = logging.getLogger('simple_example')
 logger.setLevel(logging.DEBUG)
         
 
-def evaluate_program(ast,sig=None,do_log=False):
+def evaluate_program(ast,sigma=0,do_log=False):
     """Evaluate a program as desugared by daphne, generate a sample from the prior
     Args:
         ast: json FOPPL program
@@ -39,60 +39,70 @@ def evaluate_program(ast,sig=None,do_log=False):
         defn_function_body = ast0[3]
         defn_d[defn_function_name] = [defn_function_args,defn_function_body]
         ast1 = ast[1]
-        res = evaluate(ast1,defn_d=defn_d,do_log=do_log)
+        res, sigma = evaluate(ast1,sigma,defn_d=defn_d,do_log=do_log)
     elif len(ast) == 1:
-        res = evaluate(ast0,defn_d=defn_d,do_log=do_log)
+        res, sigma = evaluate(ast0,sigma,defn_d=defn_d,do_log=do_log)
     else:
         assert False
-    return res, sig
+    return res, sigma
+
+# def log_prob(distribution, value_of_rv):
+#     return distribution.log_prob(value_of_rv)
 
 
 number = (int,float)
-def evaluate(e,local_env={},defn_d={},do_log=False):
+def evaluate(e,sigma=0,local_env={},defn_d={},do_log=False):
     # TODO: get local_env to evaluate values to tensors, not regular floats
     # remember to return evaluate (recursive)
     # everytime we call evaluate, we have to use local_env, otherwise it gets overwritten with the default {}
-    if do_log: logger.info('local_env {}'.format(local_env))
+    if do_log: logger.info('local_env {}, sigma {}'.format(local_env, sigma))
+    if do_log: logger.info('e {}, sigma {}'.format(e, sigma))
+
     # get first expression out of list or list of one
     if not isinstance(e,list) or len(e) == 1:
         if isinstance(e,list):
             e = e[0]
-
         if isinstance(e, number):
-            if do_log: logger.info('match case: number {}'.format(e))
-            return torch.tensor(float(e))
+            if do_log: logger.info('match case number: e {}, sigma {}'.format(e, sigma))
+            return torch.tensor(float(e)), sigma
         elif isinstance(e,list):
-            if do_log: logger.info('match case: list {}'.format(e))
-            return e
+            if do_log: logger.info('match case list: e {}, sigma {}'.format(e, sigma))
+            return e, sigma
         elif e in list(primitives_d.keys()):
-            if do_log: logger.info('match case: primitives_d {}'.format(e))
-            return e
+            if do_log: logger.info('match case primitives_d: e {}, sigma {}'.format(e, sigma))
+            return e, sigma
         elif e in list(distributions_d.keys()):
-            if do_log: logger.info('match case: distributions_d {}'.format(e))
-            return e
+            if do_log: logger.info('match case distributions_d: e {}, sigma {}'.format(e, sigma))
+            return e, sigma
         elif torch.is_tensor(e) and not len(list(e.shape)) == 0:
-            if do_log: logger.info('match case: is_tensor {}'.format(e))
-            return e
+            if do_log: logger.info('match case is_tensor: e {}, sigma {}'.format(e, sigma))
+            return e, sigma
         elif e in local_env.keys():
-            if do_log: logger.info('match case: local_env, e {}'.format(e))
-            if do_log: logger.info('match case: local_env, local_env[e] {}'.format(local_env[e]))
+            if do_log: logger.info('match case local_env: e {}, sigma {}'.format(e, sigma))
+            if do_log: logger.info('match case local_env: local_env[e] {}'.format(local_env[e]))
             return local_env[e] # TODO return evaluate?
         elif e in list(defn_d.keys()):
-            if do_log: logger.info('match case: defn_d {}'.format(e))
-            return e
+            if do_log: logger.info('match case defn_d: e {}, sigma {}'.format(e, sigma))
+            return e, sigma
         elif isinstance(e,distribution_types):
-            if do_log: logger.info('match case: distribution {}'.format(e))
-            return e
+            if do_log: logger.info('match case distribution: e {}, sigma {}'.format(e,sigma))
+            return e, sigma
         else:
-            assert False
+            assert False, 'case not matched'
     elif e[0] == 'sample':
-        if do_log: logger.info('match case: sample {}'.format(e))
-        distribution = evaluate(e[1],local_env,defn_d,do_log=do_log)
-        return distribution.sample() # match shape in number base case
+        if do_log: logger.info('match case sample: e {}, sigma {}'.format(e,sigma))
+        distribution, sigma = evaluate(e[1],sigma,local_env,defn_d,do_log=do_log)
+        return distribution.sample(), sigma # match shape in number base case
     elif e[0] == 'observe':
-        return None
+        e1, e2 = e[1:]
+        d1, sigma = evaluate(e1,sigma,local_env,defn_d,do_log=do_log)
+        c2, sigma = evaluate(e2,sigma,local_env,defn_d,do_log=do_log)
+        log_w = d1.log_prob(c2)
+        if do_log: logger.info('match case observe: e {}, sigma {}, log_w {}'.format(e,sigma,log_w))
+        sigma  = sigma + log_w
+        return c2, sigma
     elif e[0] == 'let': 
-        if do_log: logger.info('match case: let {}'.format(e))
+        if do_log: logger.info('match case let: e {}, sigma {}'.format(e, sigma))
         # let [v1 e1] e0
         # here 
             # e[0] : "let"
@@ -100,42 +110,43 @@ def evaluate(e,local_env={},defn_d={},do_log=False):
             # e[2] : e0
         # evaluates e1 to c1 and binds this value to e0
         # this means we update the context with old context plus {v1:c1}
-        c1 = evaluate(e[1][1],local_env,defn_d,do_log=do_log) # evaluates e1 to c1
+        c1, sigma = evaluate(e[1][1],sigma,local_env,defn_d,do_log=do_log) # evaluates e1 to c1
         v1 = e[1][0]
-        return evaluate(e[2], local_env = {**local_env,v1:c1},defn_d=defn_d,do_log=do_log)
+        return evaluate(e[2], sigma, local_env = {**local_env,v1:c1},defn_d=defn_d,do_log=do_log), sigma
     elif e[0] == 'if': # if e0 e1 e2
-        if do_log: logger.info('match case: if {}'.format(e))
-        e0 = e[1]
-        e1 = e[2]
-        e2 = e[3]
-        if evaluate(e0,local_env,defn_d,do_log=do_log):
-            return evaluate(e1,local_env,defn_d,do_log=do_log)
+        if do_log: logger.info('match case if: e {}, sigma {}'.format(e, sigma))
+        e1 = e[1]
+        e2 = e[2]
+        e3 = e[3]
+        e1_prime, sigma = evaluate(e1,sigma,local_env,defn_d,do_log=do_log)
+        if e1_prime:
+            return evaluate(e2,sigma,local_env,defn_d,do_log=do_log)
         else:
-            return evaluate(e2,local_env,defn_d,do_log=do_log) 
+            return evaluate(e3,sigma,local_env,defn_d,do_log=do_log) 
 
     else:
         cs = []
         for ei in e:
-            if do_log: logger.info('cycling through expressions. ei {}'.format(ei))
-            c = evaluate(ei,local_env,defn_d,do_log=do_log)
+            if do_log: logger.info('cycling through expressions: ei {}, sigma {}'.format(ei,sigma))
+            c, sigma = evaluate(ei,sigma,local_env,defn_d,do_log=do_log)
             cs.append(c)
         if cs[0] in primitives_d:
             if do_log: logger.info('do case primitives_d: cs0 {}'.format(cs[0]))
             if do_log: logger.info('do case primitives_d: cs1 {}'.format(cs[1:]))
             if do_log: logger.info('do case primitives_d: primitives_d[cs[0]] {}'.format(primitives_d[cs[0]]))
-            return primitives_d[cs[0]](cs[1:])
+            return primitives_d[cs[0]](cs[1:]), sigma
         elif cs[0] in distributions_d:
             if do_log: logger.info('do case distributions_d: cs0 {}'.format(cs[0]))
-            return distributions_d[cs[0]](cs[1:])
+            return distributions_d[cs[0]](cs[1:]), sigma
         elif cs[0] in defn_d:
             if do_log: logger.info('do case defn: cs0  {}'.format(cs[0]))
             defn_function_li = defn_d[cs[0]]
             defn_function_args, defn_function_body = defn_function_li
             local_env_update = {key:value for key,value in zip(defn_function_args, cs[1:])}
             if do_log: logger.info('do case defn: update to local_env from defn_d {}'.format(local_env_update))
-            return evaluate(defn_function_body,local_env = {**local_env, **local_env_update},defn_d=defn_d,do_log=do_log)
+            return evaluate(defn_function_body,sigma,local_env = {**local_env, **local_env_update},defn_d=defn_d,do_log=do_log)
         else:
-            assert False
+            assert False, 'not implemented'
 
 def get_stream(ast):
     """Return a stream of prior samples"""
