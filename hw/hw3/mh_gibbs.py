2b30e6caf82e4cddbca7d8a3a35ceac7e0bc7582
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-10-25 11:18:38 -0700

    no bugs on programs 1,2,4. results on 1 not expected 7.2

---
+++ hw/hw3/mh_gibbs.py
@@ -0,0 +1,57 @@
+import torch
+
+from evaluation_based_sampling import evaluate
+from graph_based_sampling import sample_from_joint
+from graph_based_sampling import sample_from_joint_precompute
+
+def accept(vertex,local_env,local_env_prime,A,P,do_log):
+    link_function = P[vertex]
+    e = link_function[1]
+    d_q, _ = evaluate(e,local_env=local_env,do_log=do_log)
+    d_q_prime, _ = evaluate(e,local_env=local_env_prime,do_log=do_log)
+    log_a = d_q_prime.log_prob(local_env[vertex]) - d_q.log_prob(local_env_prime[vertex])
+    for observed_vertex in A[vertex]:
+        d_p_prime = evaluate(P[observed_vertex][1],local_env = local_env_prime,do_log=do_log)[0]
+        d_p = evaluate(P[observed_vertex][1],local_env = local_env,do_log=do_log)[0]
+        log_a += d_p_prime.log_prob(local_env_prime[vertex])
+        log_a -= d_p.log_prob(local_env[vertex])
+    return torch.exp(log_a)
+
+def gibbs_step(local_env,P,A,X_sample_vertices,do_log):
+    for vertex in X_sample_vertices:
+        link_function = P[vertex]
+        e = link_function[1]
+
+        distribution, sigma = evaluate(e,local_env=local_env,do_log=do_log)
+        local_env_prime = local_env.copy()
+        local_env_prime[vertex] = distribution.sample()
+
+        alpha = accept(vertex,local_env,local_env_prime,A,P,do_log=do_log)
+        u = torch.rand(1)
+        if u < alpha:
+            local_env = local_env_prime
+    return local_env
+
+def gibbs(num_steps,local_env,P,A,X,do_log):
+    local_env_list = []
+    for step in range(num_steps):
+        local_env = gibbs_step(local_env,P,A,X,do_log=do_log)
+        local_env_list.append(local_env)
+    return local_env_list
+
+
+def mh_gibbs_wrapper(graph,num_steps,do_log):
+    G = graph[1]
+    verteces = G['V']
+    A = G['A']
+    P = G['P']
+    X = set(verteces) - set(G['Y'].keys())
+
+    verteces_topsorted = sample_from_joint_precompute(graph)
+    _, local_env = sample_from_joint(graph,verteces_topsorted=verteces_topsorted)
+    local_env_list0 = [local_env]
+
+    local_env_list = gibbs(num_steps,local_env,P,A,X,do_log=do_log)
+
+    local_env_list = local_env_list0 + local_env_list
+    return local_env_list
